2a38fe1e8c493fe61c2a6f81eff42540cd9c6789
Jeremy Cook <jeremycook0@icloud.com>
2019-11-08 16:22:36 -0500

    Added initial tests.

---
+++ java/src/test/java/dojo/supermarket/model/PackageSettings.java
@@ -0,0 +1,5 @@
+package dojo.supermarket.model;
+
+public class PackageSettings {
+    public static String ApprovalBaseDirectory = "../resources";
+}
--- java/src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ java/src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -1,43 +1,94 @@
 package dojo.supermarket.model;
 
+import dojo.supermarket.ReceiptPrinter;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
+class SupermarketTest {
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+    private ShoppingCart cart;
+    private Teller teller;
+    private ReceiptPrinter receiptPrinter;
+    private SupermarketCatalog catalog;
 
-public class SupermarketTest {
-
-    // Todo: test all kinds of discounts are applied properly
-
-    @Test
-    public void tenPercentDiscount() {
-        SupermarketCatalog catalog = new FakeCatalog();
-        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
-        catalog.addProduct(toothbrush, 0.99);
-        Product apples = new Product("apples", ProductUnit.Kilo);
-        catalog.addProduct(apples, 1.99);
-
-        Teller teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
-        ShoppingCart cart = new ShoppingCart();
-        cart.addItemQuantity(apples, 2.5);
-        
-        // ACT
-        Receipt receipt = teller.checksOutArticlesFrom(cart);
-
-        // ASSERT
-        assertEquals(4.975, receipt.getTotalPrice(), 0.01);
-        assertEquals(Collections.emptyList(), receipt.getDiscounts());
-        assertEquals(1, receipt.getItems().size());
-        ReceiptItem receiptItem = receipt.getItems().get(0);
-        assertEquals(apples, receiptItem.getProduct());
-        assertEquals(1.99, receiptItem.getPrice());
-        assertEquals(2.5*1.99, receiptItem.getTotalPrice());
-        assertEquals(2.5, receiptItem.getQuantity());
-
+    @BeforeEach
+    void setUp() {
+        catalog = new FakeCatalog();
+        cart = new ShoppingCart();
+        teller = new Teller(catalog);
+        receiptPrinter = new ReceiptPrinter();
     }
 
+    @Test
+    void tenPercentDiscount() {
+        var rice = new Product("rice", ProductUnit.Each);
+        catalog.addProduct(rice, 2.49);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, rice, 10.0);
+        cart.addItem(rice);
 
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void twentyPercentDiscount() {
+        var apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, apples, 20.0);
+        cart.addItemQuantity(apples, 2.5);
+
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void twoForSpecialPrice() {
+        var cherryTomatoes = new Product("cherry tomatoes", ProductUnit.Each);
+        catalog.addProduct(cherryTomatoes, 0.69);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, cherryTomatoes, 0.99);
+        cart.addItemQuantity(cherryTomatoes, 2);
+
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void fiveForSpecialPrice() {
+        var toothpaste = new Product("toothpaste", ProductUnit.Each);
+        catalog.addProduct(toothpaste, 1.79);
+        teller.addSpecialOffer(SpecialOfferType.FiveForAmount, toothpaste, 7.49);
+        cart.addItemQuantity(toothpaste, 5);
+
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void twoForOne() {
+        var toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        teller.addSpecialOffer(SpecialOfferType.TwoForAmount, toothbrush, 0.99);
+        cart.addItemQuantity(toothbrush, 2);
+
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
+
+    @Test
+    void threeForTwo() {
+        var toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        teller.addSpecialOffer(SpecialOfferType.ThreeForTwo, toothbrush, 0.99);
+        cart.addItemQuantity(toothbrush, 3);
+
+        var result = receiptPrinter.printReceipt(teller.checksOutArticlesFrom(cart));
+
+        Approvals.verify(result);
+    }
 }
